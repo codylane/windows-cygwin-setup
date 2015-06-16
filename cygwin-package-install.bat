421003f6c88b2cfb1d1c6ba43928e1b00f591865
codylane <cody.lane@hp.com>
2015-06-15 19:00:23 -0600

    fixing a copy and paste from windows to VI

--- cygwin-package-install.bat
+++ cygwin-package-install.bat
@@ -1,21 +1,22 @@
 @ECHO OFF
 REM -- Automates cygwin installation
 
-REM SETLOCAL
+SETLOCAL
 REM REM -- Change to the directory of the executing batch file
-REM cd %HOMEPATH%\Downloads
+cd %HOMEPATH%\Downloads
 
-REM SET SITE=http://cygwin.mirrors.pair.com/
-REM SET LOCALDIR=%CD%
-REM SET ROOTDIR=C:/cygwin
+SET SITE=http://cygwin.mirrors.pair.com/
+SET LOCALDIR=%CD%
+SET ROOTDIR=C:/cygwin
 
-REM SET PACKAGES=bash,bash-completion,openssh,rxvt,python,git,wget,curl,vim,gcc-core,make
+SET PACKAGES=bash,bash-completion,openssh,rxvt,python,git,wget,curl,vim,gcc-core,make
 
 if defined PROXY (
   REM -- installing with proxy enabled
+  echo Installing with proxy
   setup-x86.exe --disable-buggy-antivirus -q -p "%PROXY%" -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P "%PACKAGES%"
 ) else (
   REM -- installing without proxy 
-  echo Installting without proxy
+  echo Installing without proxy
   setup-x86.exe --disable-buggy-antivirus -q -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P "%PACKAGES%"
 )
